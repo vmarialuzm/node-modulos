99529d740b7576e6488c2163bdd850fd3dc093bf
vmarialuz <vmarialuzm@gmail.com>
2023-08-16 19:50:00 -0500

    moduleJS (ESM)

---
+++ index.js
@@ -0,0 +1,5 @@
+import { eval_number, saludo } from "./module.js";
+
+saludo();
+eval_number(7);
+eval_number(6);
---
+++ module.js
@@ -0,0 +1,12 @@
+export function eval_number(num) {
+    if (num % 2) {
+        console.log(`${num} es un número impar`)
+    }
+    else {
+        console.log(`${num} es un número par`)
+    }
+}
+
+export function saludo() {
+    console.log("Hola")
+}
